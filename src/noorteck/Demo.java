14c9108e064ca86aa66388693d6969b14d0727d3
shahl <shahl@192.168.1.239>
2021-10-01 15:20:32 -0400

    please work

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 	
 	public static void main(String[]args) {
 		System.out.println("GitHub Practice");
-		System.out.println("please work");
+		System.out.println("please workkkkkkk");
 	}
 
 }
